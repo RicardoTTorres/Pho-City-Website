fa00f5dc8aed44e039d97dd55b07e67d7d74a69f
Mukesh <msmehmi@csus.edu>
2025-10-17 13:32:49 -0700

    Setup Page Routing
    Added page routes for Home, About, Menu, and Contact for NavigationBar and Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,24 @@ import { Navbar } from "@/sections/Navbar";
 import { ContentProvider } from "@/context/ContentContext";
 import { Footer } from "@/sections/Footer";
 import { footerConfig } from "@/config/footer.config";
+import {Routes, Route} from 'react-router-dom';
 import Home from "@/pages/Home";
+import About from "@/pages/About";
+import Contact from "@/pages/Contact";
+import AdminLogin from "@/pages/AdminLogin";
+import Menu from "./pages/Menu";
 
 export default function App() {
   return (
     <ContentProvider>
       <Navbar />
-      <Home />  
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/menu" element={<Menu />}/>
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/adminlogin" element={<AdminLogin />} />
+        </Routes>
       <Footer config={footerConfig} />
     </ContentProvider>
   );
