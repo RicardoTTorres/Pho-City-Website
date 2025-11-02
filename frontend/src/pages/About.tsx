17754bcf0a604f3e99cce5d89b1891581dbb1b67
Kwin Lee <kwinlee@outlook.com>
2025-11-02 01:56:41 -0800

    PC-58 Added "aboutUs" image to the assets folder and added the image to the about us page.

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,7 +1,17 @@
+import { useContent } from "@/context/ContentContext";
+import aboutUs from "@/assets/aboutUs.png";
+
 export default function About() {
+    const { content } = useContent();
+    
     return (
         <>
             <h1>About Page</h1>
+            <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <img src = {aboutUs} 
+            alt = "About Us"
+            style={{ width: "650px", height: "auto", marginRight: "50px" }} />
+            </div>
         </>
     );
 }
