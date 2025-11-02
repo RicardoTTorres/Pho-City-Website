6847efd847cacb9375583525084acfdd0e8722df
ryanp4096 <ryanp4096@gmail.com>
2025-11-01 18:28:12 -0700

    PC-40 about us page data is fetched from backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import authRoutes from './routes/auth.js';
 import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.js';
@@ -14,15 +13,22 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(cookieParser());
+app.use((req, res, next) => {
+  console.log("Incoming request:", req.method, req.url, "Origin:", req.get("Origin"));
+  next();
+});
 
 const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN || "http://localhost:5173";
-
 app.use(cors({
   origin: FRONTEND_ORIGIN,   
   credentials: true          
 }));
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+
 
 // Routes
 app.use('/api/contact', contactRoutes);
--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,7 +1,15 @@
+import { useContent } from "@/context/ContentContext";
+
 export default function About() {
+    const { content } = useContent();
+    const about = content.about
+    
+    // this is very bare bones just to test the backend for the about page.
+    // frontend for about page still needs to be coded
     return (
         <>
-            <h1>About Page</h1>
+            <h1>{about.title}</h1>
+            <p>{about.content}</p>
         </>
     );
 }
