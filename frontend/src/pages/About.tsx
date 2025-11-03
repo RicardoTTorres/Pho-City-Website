67f8ae89d547dc4259585e7d0bbf9513ebe96943
Kwin Lee <kwinlee@outlook.com>
2025-11-02 17:06:57 -0800

    PC-58 Added text to the page and formated image and text to fit together.

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -5,13 +5,33 @@ export default function About() {
     const { content } = useContent();
     
     return (
-        <>
-            <h1>About Page</h1>
+        
+        /*Formats Text and Image*/
+        <div
+            style={{
+                display: "flex",
+                alignItems: "center",      
+                justifyContent: "space-between",
+                padding: "50px",           
+      }}>
+
+        <div style={{ maxWidth: "45%"}}>
+            
+            {/*Header With Space at the bottom*/}
+            <h1 style={{ marginBottom: "50px"}}>Pho City: Where Vietnamese Cuisine Transends Expectations!</h1>
+
+            {/*About Us content from ContentContext.tsx*/}
+            <p>
+                {content.about.content}
+            </p>
+            </div>
+            
+            {/*Image*/}
             <div style={{ display: "flex", justifyContent: "flex-end" }}>
             <img src = {aboutUs} 
             alt = "About Us"
-            style={{ width: "650px", height: "auto", marginRight: "50px" }} />
+            style={{ width: "650px", height: "auto"}} />
             </div>
-        </>
+        </div>
     );
 }
