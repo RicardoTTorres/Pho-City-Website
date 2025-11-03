e28483d7c85aa8b5a1dd4e327e1610187dd29065
Shika <skandel2@csus.edu>
2025-11-02 18:23:30 -0800

    PC 41 Preview and Save changes as admin

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,16 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
 import cookieParser from 'cookie-parser';
-
+import contactRoutes from './routes/contactRoutes.js';
+import adminContactRoutes from './routes/adminContactRoutes.js';
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 app.use(express.json());
 app.use(cookieParser());
-
 
 const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN || "http://localhost:5173";
 
@@ -23,14 +23,13 @@ app.use(cors({
 
 // Routes
 app.use('/api/contact', contactRoutes);
-
+app.use('/api/admin/contact', adminContactRoutes);
+app.use('/api/admin', authRoutes);  
 
 // Root test route
 app.get('/', (req, res) => {
   res.send('Hi from server!');
 });
-
-app.use('/api/admin', authRoutes);  
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
