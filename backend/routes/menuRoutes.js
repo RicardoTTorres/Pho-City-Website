8ef771bbe9a946e4b600c8ad9baaeee23d3adf4e
ryanp4096 <ryanp4096@gmail.com>
2025-11-04 12:05:04 -0800

    PC-90 PC-91 PC-92 Edit Menu API

--- backend/routes/menuRoutes.js
+++ backend/routes/menuRoutes.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { getMenu } from "../controllers/menuController.js";
+import { getMenu, addCategory, editCategory, deleteCategory, addItem, editItem, deleteItem } from "../controllers/menuController.js";
 
 const router = Router();
 
 router.get("/", getMenu);
+router.post("/categories", addCategory);
+router.put("/categories/:id", editCategory);
+router.delete("/categories/:id", deleteCategory);
+router.post("/items", addItem);
+router.put("/items/:id", editItem);
+router.delete("/items/:id", deleteItem);
 
 export default router;
