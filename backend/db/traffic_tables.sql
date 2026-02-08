7b26db2212f403779243fcb15e48ae5f457cd8aa
ryanp4096 <ryanp4096@gmail.com>
2026-02-08 02:33:30 -0800

    PC-104 final bug fixes

--- backend/db/traffic_tables.sql
+++ backend/db/traffic_tables.sql
@@ -8,12 +8,27 @@ CREATE TABLE `traffic_dates` (
   PRIMARY KEY (`date`)
 );
 
+INSERT INTO `traffic_dates` VALUES ("2025-11-10", 75);
+INSERT INTO `traffic_dates` VALUES ("2025-11-11", 120);
+INSERT INTO `traffic_dates` VALUES ("2025-11-12", 95);
+INSERT INTO `traffic_dates` VALUES ("2025-11-13", 180);
+INSERT INTO `traffic_dates` VALUES ("2025-11-14", 160);
+INSERT INTO `traffic_dates` VALUES ("2025-11-15", 130);
+
+
 DROP TABLE IF EXISTS `traffic_pages`;
 CREATE TABLE `traffic_pages` (
   `page` VARCHAR(255) NOT NULL,
   `page_views` INT NOT NULL,
   PRIMARY KEY (`page`)
 );
+
+INSERT INTO `traffic_pages` VALUES ("/", 420);
+INSERT INTO `traffic_pages` VALUES ("/about", 180);
+INSERT INTO `traffic_pages` VALUES ("/contact", 110);
+INSERT INTO `traffic_pages` VALUES ("/menu", 500);
+INSERT INTO `traffic_pages` VALUES ("/stats", 38);
+
 
 DROP TABLE IF EXISTS `traffic_visitors`;
 CREATE TABLE `traffic_visitors` (
