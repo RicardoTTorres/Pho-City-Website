40935bc2bfa3bce83be1f2ae4ad555be6d760d22
Mukesh <msmehmi@csus.edu>
2026-02-07 20:55:18 -0800

    PC-106: Fixed missing putFooter import in footerRoutes.js

--- backend/routes/footerRoutes.js
+++ backend/routes/footerRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {getFooter} from "../controllers/footerController.js";
+import { putFooter } from "../controllers/footerController.js";
 
 const router = express.Router();
 
