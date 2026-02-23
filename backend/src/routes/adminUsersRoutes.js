e943b3ce788cdafcb4ebe638fdd5e4392b31a629
ColeWood1 <thecobaltian1@gmail.com>
2026-02-22 21:41:42 -0800

    Add files via upload

--- backend/src/routes/adminUsersRoutes.js
+++ backend/src/routes/adminUsersRoutes.js
@@ -1,10 +1,10 @@
-// src/routes/adminUsersRoutes.js
-import express from "express";
-import { getAdminUsers } from "../controllers/adminUsersController.js";
+import express from 'express';
+import { getAdminUsers, createAdminUser } from '../controllers/adminUsersController.js'
 import { requireAuth } from "../middleware/requireAuth.js";
 
 const router = express.Router();
 
 router.use(requireAuth);
-router.get("/", getAdminUsers);
+router.get('/', getAdminUsers);
+router.put('/', createAdminUser);
 export default router;
