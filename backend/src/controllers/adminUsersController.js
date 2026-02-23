1799631b377058f707f9c2f17077b301dd7b9822
ColeWood1 <thecobaltian1@gmail.com>
2026-02-22 21:38:52 -0800

    Add files via upload

--- backend/src/controllers/adminUsersController.js
+++ backend/src/controllers/adminUsersController.js
@@ -1,5 +1,5 @@
-// src/controllers/adminUserController.js
 import { pool } from "../db/connect_db.js";
+import bcrypt from "bcrypt";
 
 export async function getAdminUsers(req, res) {
   try {
@@ -13,5 +13,28 @@ export async function getAdminUsers(req, res) {
   } catch (err) {
     console.error("Error fetching admin users:", err);
     res.status(500).json({ error: "Failed to load admin users" });
+  }
+}
+
+export async function createAdminUser(req, res) {
+  try {
+    const {new_email, new_password} = req.body;
+
+    if (!new_email || !new_password) {
+      return res
+        .status(400)
+        .json({message: "Email and password are required."});
+    }
+
+    const passwordHash = await bcrypt.hash(new_password, 10);
+    await pool.query(
+      "INSERT INTO admins (email, password_hash, role) VALUES (?, ?, 'admin')",
+      [new_email, passwordHash],
+    );
+
+    res.json({message: "Created new admin user"});
+  } catch (error) {
+    console.error("Error creating new admin user:", error);
+    res.status(500).json({message: "Internal Server Error"});
   }
 }
